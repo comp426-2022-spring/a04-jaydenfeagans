12a1bfb4b76419200c9ca71639eb2c6fdc31a654
jfea40 <jfea40@gmail.com>
2022-04-28 21:30:50 -0400

    6

--- database.js
+++ database.js
@@ -1,38 +1,18 @@
+const database = require('better-sqlite3')
 
-"use strict";
+const logdb = new database('log.db')
 
-const Database = require('better-sqlite3');
-
-const db = new Database('log.db');
-
-
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
-
-
-
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 let row = stmt.get();
 
 if(row === undefined) {
-    console.log('Log database missing');
+    console.log('Log database missing')
 
-    const sqlInit = `
-    CREATE TABLE accesslog ( 
-        id INTEGER PRIMARY KEY, 
-        remoteaddr TEXT,
-        remoteuser TEXT,
-        time TEXT,
-        method TEXT,
-        url TEXT,
-        protocol TEXT,
-        httpversion TEXT,
-        status TEXT, 
-        referrer TEXT,
-        useragent TEXT
-    );
-    `;
-    db.exec(sqlInit)
+    const sqlInit = `CREATE TABLE accesslog ( id INTEGER NOT NULL PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time INTEGER, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status INTEGER, referer TEXT, useragent TEXT);`;
+
+    logdb.exec(sqlInit)
 } else {
-    console.log('Database exists.')
+    console.log('Log database exists.')
 }
 
-module.exports = db
+module.exports = logdb
